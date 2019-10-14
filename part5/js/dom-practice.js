c98213d7e846d001fe13f10308725bb8e2899ee2
Vaibhav Wadikar <vaibhavwadikar8@gmail.com>
2019-10-13 19:50:35 -0700

    Added part 5

--- part5/js/dom-practice.js
+++ part5/js/dom-practice.js
@@ -1,12 +1,33 @@
-/*eslint-env browser*/
+//eslint-env browser/
+//jslint-env browser/
 
 //STEP 1
+function clickMe(){
+    window.alert("I have been clicked");
+}
 
 //STEP 2
+var el = window.document.getElementById("clickButton");
+el.onclick = function () {
+    window.alert("I have been clicked");
+}
 
 //STEP 3
+var elBtnListener = window.document.getElementById("elBtnListener");
+elBtnListener.addEventListener("click", clickMe);
 
 //STEP 4
+var elBtnListenerCB = window.document.getElementById("elBtnListenerCB");
+elBtnListenerCB.addEventListener("click", function () {
+    window.alert("I have been clicked");
+});
 
 //STEP 5
+function init() {
+    var elBtnListenerCB1 = window.document.getElementById("elBtnListenerCB1");
+    elBtnListenerCB1.addEventListener("click", function () {
+        window.alert("I have been clicked");
+    });
+}
 
+window.addEventListener("load", init);
